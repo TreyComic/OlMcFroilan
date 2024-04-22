219dd887aa56a3343b372e43d523c745fd1c4a12
JRThompson <JRThompson@wil.yearup.org>
2024-04-21 20:04:54 -0400

    diddled with stuff related to pilots and flying.

--- src/main/java/demo/Pilot.java
+++ src/main/java/demo/Pilot.java
@@ -2,13 +2,23 @@ package demo;
 
 public class Pilot extends Person
 {
+    private Cropduster personalPlane;
     public Pilot(String name)
     {
         super(name);
+        personalPlane=new Cropduster();
     }
     @Override
     public void mount(Rideable riddenObject)
     {
-        super.setMount(riddenObject);
+        super.mount(riddenObject);
+    }
+    public void mount()
+    {
+        super.setMount(personalPlane);
+    }
+    public void mount(Aircraft airplane)
+    {
+        super.setMount(airplane);
     }
 }
