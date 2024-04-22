4002d56ab4275ebb1a88f86865c751388486e84d
JRThompson <JRThompson@wil.yearup.org>
2024-04-22 14:54:07 -0400

    functions to fillStables and chickencoops.

--- src/main/java/demo/ChickenCoop.java
+++ src/main/java/demo/ChickenCoop.java
@@ -2,17 +2,47 @@ package demo;
 
 public class ChickenCoop
 {
-    Chicken[] inhabitants;
+    Chicken[] chickens;
     public ChickenCoop(int capacity)
     {
-        inhabitants = new Chicken[capacity];
+        chickens = new Chicken[capacity];
+        System.out.println("Coop constructed");
     }
     public Chicken[] getChickens()
     {
-        return inhabitants;
+        return chickens;
     }
     public void setChickens(Chicken[] replacements)
     {
-        inhabitants = replacements;
+        chickens = replacements;
+    }
+    public Chicken getChicken(int chickenLocation)
+    {
+        return chickens[chickenLocation];
+    }
+    public void setHorse(Chicken chicken,int chickenLocation)
+    {
+        this.chickens[chickenLocation] = chicken;
+    }
+    public void fillCoop(int chickenFill)
+    {
+        int counter=0;
+        for(int i=0;i<chickenFill;i++)
+        {
+            chickens[i]=new Chicken();
+            counter++;
+        }
+        System.out.println("Coop filled with "+counter+" chickens.");
+
+    }
+    public void fillCoop()
+    {
+        int counter=0;
+        for(int i=0;i<chickens.length;i++)
+        {
+            chickens[i]=new Chicken();
+            counter++;
+        }
+        System.out.println("Stable filled with "+counter+" horses.");
     }
 }
--- src/main/java/demo/Stable.java
+++ src/main/java/demo/Stable.java
@@ -24,4 +24,24 @@ public class Stable
         System.out.println("Stable contstructed");
         this.horses=new Horse[horseCapacity];
     }
+    public void fillStable(int horseFill)
+    {
+        int counter=0;
+        for(int i=0;i<horseFill;i++)
+        {
+            horses[i]=new Horse();
+            counter++;
+        }
+        System.out.println("Stable filled with "+counter+" horses.");
+    }
+    public void fillStable()
+    {
+        int counter=0;
+        for(int i=0;i<horses.length;i++)
+        {
+            horses[i]=new Horse();
+            counter++;
+        }
+        System.out.println("Stable filled with "+counter+" horses.");
+    }
 }
